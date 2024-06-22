8d68a4778a940536fecaf0ed7b9ee1a6f941a032
Durga Chikkala <69903252+Durga-chikkala@users.noreply.github.com>
2024-06-22 15:10:54 +0530

    converted from browserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import { ContentProvider } from './components/Context/Context';
 import { AuthProvider } from './components/Context/AuthContext';
 
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
   <ContentProvider>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </ContentProvider>
   </AuthProvider>
 );
