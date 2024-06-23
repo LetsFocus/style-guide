8bad477cd67e8639a7000d0e1675060153f80acc
Durga Chikkala <69903252+Durga-chikkala@users.noreply.github.com>
2024-06-23 05:55:00 +0000

    revised mobile version code

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,18 +6,20 @@ import './Login.css'
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleLogin = async () => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
     } catch (error) {
-      console.error("Error logging in:", error);
+      setError(error.message);
     }
   };
 
   return (
     <div className='login'>
       <h2>Login with Email/Password</h2>
+      {error && <p style={{ color: 'red',maxWidth:"100%" }}>{error}</p>}
       <div className="login-signup">
         <input
             type="email"
